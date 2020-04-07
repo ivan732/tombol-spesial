d6dab4dc96d547b830871597006374a7875daac2
ivan732 <63299326+ivan732@users.noreply.github.com>
2020-04-07 21:28:07 +0700

    Update tombol-spesial.sh

--- tombol-spesial.sh
+++ tombol-spesial.sh
@@ -1,7 +1,4 @@
 clear
-ulang="y"
-while [ $ulang = "y" ]
-do
 echo "
 \033[0;31m ╔══╗╔═╗╔═╗╔══╗╔══╗╔══╗╔╗─
  ║══╣║╬║║╦╝║══╣╚║║╝║╔╗║║║─
@@ -12,22 +9,5 @@ echo "
  CHANEL YT: Lerry Real
 \033[1;34m╚══════════════════════════╝
 \033[1;37m"
-read -p "Tambahkan Tombol (ya/tidak) : " uwi
 
-if [ $uwi = "ya" ]                                                                         then
-then
-
-        python2 jgn_recode
-
-elif [ $uwi = "tidak" ]
-then
-
-        echo "\033[1;31mGOODBYE..."
-
-exit
-else
-
-        echo "\033[1;36m[\033[1;32m!\033[1;36m]\033[1;31m INPUT SALAH..."
-
-fi
-done
+python2 jgn_recode
